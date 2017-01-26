35c10155df3060466fd7a92376de2699d19e6106
JoseRoberts87 <robertslafamilia@outlook.com>
2017-01-26 14:21:50 -0500

    added html elements

--- main.py
+++ main.py
@@ -15,10 +15,18 @@
 # limitations under the License.
 #
 import webapp2
+# import caesar
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+    	encrypted_message = ''
+    	# caesar.encrypt("this", 13)
+
+    	textarea = "<textarea>" + encrypted_message + "</textarea>"
+    	submit = "<input type= 'submit' />"
+    	form = "<form>" + textarea + "<br>" + submit + "</form>"
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
